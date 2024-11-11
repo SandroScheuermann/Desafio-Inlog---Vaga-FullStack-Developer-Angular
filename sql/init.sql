104919a4880d32e29b1c22681722fce3036dbee7
Sandro <sandroryze@gmail.com>
2024-11-11 20:19:37 -0300

    ajustes no init e no appsettings para rodar em compose

--- sql/init.sql
+++ sql/init.sql
@@ -3,39 +3,42 @@ GO
 USE TesteInlog;
 GO
 
-CREATE TABLE TiposVeiculo (
+-- Criação da tabela TipoVeiculo
+CREATE TABLE TipoVeiculo (
     Id INT PRIMARY KEY,               
     Descricao NVARCHAR(50) NOT NULL   
 );
 
-INSERT INTO TiposVeiculo (Id, Descricao) VALUES 
+INSERT INTO TipoVeiculo (Id, Descricao) VALUES 
 (1, 'Onibus'),
 (2, 'Caminhao');
 
+-- Criação da tabela Veiculos
 CREATE TABLE Veiculos (
     Id INT IDENTITY PRIMARY KEY,
     Chassi NVARCHAR(50) NOT NULL,
     Placa NVARCHAR(20) NOT NULL,
-    TiposVeiculo INT NOT NULL,             
+    IdTipoVeiculo INT NOT NULL,             
     Cor NVARCHAR(30) NOT NULL,
-    CONSTRAINT FK_Veiculo_TiposVeiculo FOREIGN KEY (TiposVeiculo) REFERENCES TiposVeiculo(Id)
+    CONSTRAINT FK_Veiculos_TipoVeiculo FOREIGN KEY (IdTipoVeiculo) REFERENCES TipoVeiculo(Id)
 );
 
+-- Criação da tabela TelemetriasHistorico
 CREATE TABLE TelemetriasHistorico (
     Id INT IDENTITY PRIMARY KEY,
     IdVeiculo INT NOT NULL,
     DataHora DATETIME NOT NULL,
     Latitude FLOAT NOT NULL,
     Longitude FLOAT NOT NULL,
-    CONSTRAINT FK_TelemetriaHistorico_Veiculo FOREIGN KEY (IdVeiculo) REFERENCES Veiculo(Id)
+    CONSTRAINT FK_TelemetriasHistorico_Veiculos FOREIGN KEY (IdVeiculo) REFERENCES Veiculos(Id)
 );
 
+-- Criação da tabela Telemetrias
 CREATE TABLE Telemetrias (
     Id INT IDENTITY PRIMARY KEY,
     IdVeiculo INT NOT NULL,
     DataHora DATETIME NOT NULL,
     Latitude FLOAT NOT NULL,
     Longitude FLOAT NOT NULL,
-    CONSTRAINT FK_Telemetria_Veiculo FOREIGN KEY (IdVeiculo) REFERENCES Veiculo(Id)
+    CONSTRAINT FK_Telemetrias_Veiculos FOREIGN KEY (IdVeiculo) REFERENCES Veiculos(Id)
 );
-
